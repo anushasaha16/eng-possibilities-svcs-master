3d0b57ba3f4912b1dd1a7b212a62a00a4df6c0cd
Anusha Saha <44592491+anushasaha16@users.noreply.github.com>
2021-02-28 02:10:40 -0600

    Update InvestingForecastService.java

--- src/main/java/com/investing/forecastbackend/service/InvestingForecastService.java
+++ src/main/java/com/investing/forecastbackend/service/InvestingForecastService.java
@@ -20,7 +20,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Collections.emptyList;
 
@@ -38,20 +40,42 @@ public class InvestingForecastService {
         // TODO read investment options from investment-details.json
     	ObjectMapper objectMapper = new ObjectMapper();
     	objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-    	List<InvestmentDetail> details = objectMapper.readValue(new File("src/main/resources/data/investment-details.json"), new TypeReference<List<InvestmentDetail>>(){});
-    	return details;
+    	List<InvestmentDetail> details = (ArrayList) objectMapper.readValue(new File("src/main/resources/data/investment-details.json"), Map.class).get("Investments");
+    	String str = objectMapper.writeValueAsString(details);
+    	return objectMapper.readValue(str, new TypeReference<List<InvestmentDetail>>() {});
     }
 	
     public ForecastResponse getInvestmentOptions(final ForecastRequest request) {
         // TODO write algorithm to calculate investment forecast from request configuration
-    	List<Double> response = new ArrayList<Double>();
-    	for(int i = 0; i < 10; i++)
-    	{
-    		
-    	}
+    	List<InvestmentDetail> details = getInvestmentOptions();
+    	List<Double> response = getForeCast(request.getRequest(), details);
     	ForecastResponse forecastResponse = new ForecastResponse();
     	forecastResponse.setResponse(response);
         return forecastResponse;
     }
+    
+    public List<Double> getForeCast(Map<String, Double> userRequest, List<InvestmentDetail> details) {
+    	Map<Integer, Double> totalYearAmount = new HashMap<>();
+        for (InvestmentDetail i : details) {
+            //user input for category i
+            double userInvestmentPercentage = userRequest.get(i.getCategory());
+            double userInvestmentDollars = (userInvestmentPercentage / 100) * 10000;
+            for (int x = 0; x < 10; x++) {
+
+                //historical interest data for category i in year x
+                double historicalInterest = Double.valueOf(i.getData().get(x));
+                double currentInterest = (historicalInterest / 100) * userInvestmentDollars;
+
+                userInvestmentDollars = userInvestmentDollars + currentInterest;
+
+                Double currentYearTotal = totalYearAmount.getOrDefault(x, 0.0);
+                //add total amount for category i in year x in Map<Integer, Double> totalYearAmount
+                //continuously sum total for each investment i in year x
+                totalYearAmount.put(x, currentYearTotal + userInvestmentDollars);
+            }
+        }
+        return new ArrayList<>(totalYearAmount.values());
+    }
+    
 
 }
