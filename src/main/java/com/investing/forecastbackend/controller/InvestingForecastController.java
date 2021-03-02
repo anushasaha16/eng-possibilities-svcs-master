507adb4b6f636a6895736d830e6115d6fd9513e2
Anusha Saha <44592491+anushasaha16@users.noreply.github.com>
2021-03-01 22:41:02 -0600

    Update InvestingForecastController.java

--- src/main/java/com/investing/forecastbackend/controller/InvestingForecastController.java
+++ src/main/java/com/investing/forecastbackend/controller/InvestingForecastController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/forecast")
 @AllArgsConstructor
